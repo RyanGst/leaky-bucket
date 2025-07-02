ee892040589399955fe12d9e30c02313f66a7e9e
ryangst <ryangst.hire@gmail.com>
2025-07-01 21:49:39 -0300

    feat: add Redis configuration to environment and update Redis client initialization

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,8 @@
 import { RedisClient } from 'bun'
 
-export const redis = new RedisClient('redis://redis:6379', {
+const redisUrl = process.env.REDIS_URL!
+
+export const redis = new RedisClient(redisUrl, {
 	connectionTimeout: 1000,
 	maxRetries: 1
 })
